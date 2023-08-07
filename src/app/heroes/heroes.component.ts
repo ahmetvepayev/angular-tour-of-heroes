efe74f17b0eb40e0e993579a4043c413330acb5d
Ahmet Vepayev <ahmetwepaogly@gmail.com>
2023-08-07 20:01:15 +0300

    Initial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+    constructor(private heroService: HeroService) {
+        
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    heroes: Hero[] = [];
+
+    private getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroesFromService => this.heroes = heroesFromService);
+    }
+}
